dc6fcfad506d04ca39a0796a000fbb950d0e9f9d
imparang <jeongjaeyn@gmail.com>
2021-12-16 23:16:09 +0900

    feat: Create Profile component

---
+++ client/src/component/profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+
+const Profile = () => {
+  return (
+    <Container style={{ height: '626px' }}>
+      <h2>마이 페이지</h2>
+    </Container>
+  )
+}
+
+export default Profile
