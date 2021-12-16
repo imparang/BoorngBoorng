036ee1d01c2c856a16796d14c147b1f8bda5f0ce
imparang <jeongjaeyn@gmail.com>
2021-12-16 23:15:10 +0900

    feat: Create History  component

--- client/src/component/history/History.js
+++ client/src/component/history/History.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Container } from 'reactstrap'
+import React from "react";
 
-const History = props => {
+const History = (props) => {
   return (
-    <Container style={{ height: '626px' }}>
+    <div>
       <h2>사용자 구매내역</h2>
-    </Container>
-  )
-}
+    </div>
+  );
+};
 
-export default History
+export default History;
