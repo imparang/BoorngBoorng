6acf71d33cf7a0cb23809978e84d7262cf9bd0ed
imparang <jeongjaeyn@gmail.com>
2021-12-16 23:17:14 +0900

    feat: Add Routes at App.js

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,35 +1,40 @@
-import { Route } from "react-router-dom";
-import Header from "../route/Header";
-import Footer from "../route/Footer";
-import "bootstrap/dist/css/bootstrap.css";
-import "../css/toy.css";
+import { Route } from 'react-router-dom'
+import Header from '../route/Header'
+import Footer from '../route/Footer'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../css/toy.css'
 
-import Board from "../component/board/Board";
-import Login from "../component/login/Login";
-import Register from "../component/register/Register";
-import Product from "../component/product/Product";
-import Cart from "../component/cart/Cart";
-import History from "../component/history/History";
-import Naver from "./naver/Naver";
-import { useState } from "react";
+import Board from '../component/board/Board'
+import Login from '../component/login/Login'
+import Register from '../component/register/Register'
+import Product from '../component/product/Product'
+import Cart from '../component/cart/Cart'
+import History from '../component/history/History'
+import Naver from './naver/Naver'
+import { useState } from 'react'
+import HomeAnimation from './HomeAnimation'
+import { Container } from 'reactstrap'
+import Profile from './profile/Profile'
 
 function App() {
-  const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useState('')
 
   return (
-    <div className="App">
+    <Container>
       <Header />
-      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={HomeAnimation} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/naverApi" component={Naver} />
       <Route exact path="/board" component={Board} />
       <Route path="/product" render={() => <Product userId={userId} />} />
       <Route path="/cart" render={() => <Cart userId={userId} />} />
+      <Route path="/profile" render={() => <Profile />} />
       <Route path="/history" render={() => <History userId={userId} />} />
+
       <Footer />
-    </div>
-  );
+    </Container>
+  )
 }
 
-export default App;
+export default App
