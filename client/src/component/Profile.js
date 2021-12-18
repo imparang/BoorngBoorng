9dbdc050bf5d250c3db2d26f68b659c258a866db
imparang <jeongjaeyn@gmail.com>
2021-12-18 15:36:38 +0900

    feat: Create Mypage router

--- client/src/component/Profile.js
+++ client/src/component/Profile.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap'
 
 const Profile = () => {
   return (
-    <Container style={{ height: '626px' }}>
+    <Container style={{ height: '600px', marginBottom: '16px' }}>
       <h2>마이 페이지</h2>
     </Container>
   )
