22ef1e04cf7e474898817866e9942b6a20b8e8cd
imparang <jeongjaeyn@gmail.com>
2021-12-18 15:39:04 +0900

    feat: Implement home header

--- client/src/route/Header.js
+++ client/src/route/Header.js
@@ -1,36 +1,80 @@
 import React, { Component } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap'
+import { Form, Input, Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap'
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <Navbar>
-          <NavbarBrand>
-            <Link
-              to={'/'}
-              style={{ fontSize: '48px', lineHeight: '1.1', fontWeight: '700' }}
+        <Navbar
+          style={{
+            borderBottom: '1px solid #ccc',
+            marginBottom: '16px',
+            justifyContent: 'space-between'
+          }}
+        >
+          <NavLink
+            to="/"
+            style={{ display: 'block', padding: '0 20px 0 0', margin: 0 }}
+          >
+            <h1>
+              Arch <br />
+              BESHOP
+            </h1>
+          </NavLink>
+          <Nav
+            className="mr-auto menu"
+            style={{ margin: 0, justifyContent: 'flex-end', flex: 1 }}
+          >
+            <NavItem
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                flex: 1
+              }}
             >
-              <h1>
-                Arch <br />
-                BESHOP
-              </h1>
-            </Link>
-          </NavbarBrand>
-          {/* <NavbarToggler /> */}
-          <Nav className="mr-auto menu">
-            <NavItem>
-              <NavLink to={'/board'}>게시판</NavLink>
+              <Form style={{ width: '100%', maxWidth: '300px' }}>
+                <Input placeholder="상품을 검색하세요" block="true" />
+              </Form>
             </NavItem>
-            <NavItem>
-              <NavLink to={'/cart'}>장바구니</NavLink>
+            <NavItem
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <NavLink style={{ textAlign: 'center' }} to="/board">
+                게시판
+              </NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink to={'/profile'}>마이페이지</NavLink>
+            <NavItem
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <NavLink to="/cart">장바구니</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink to={'/naverApi'}>상품등록</NavLink>
+            <NavItem
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <NavLink to="/profile">마이페이지</NavLink>
+            </NavItem>
+            <NavItem
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <NavLink to="/naverApi">상품등록</NavLink>
             </NavItem>
           </Nav>
         </Navbar>
