59a072e2d17fdc9826c9e4923131b76e4f47c85d
imparang <jeongjaeyn@gmail.com>
2021-12-18 15:37:27 +0900

    feat: Create Product router

--- client/src/component/Product.js
+++ client/src/component/Product.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap'
 
 const Product = props => {
   return (
-    <Container style={{ height: '626px' }}>
+    <Container style={{ height: '600px', marginBottom: '16px' }}>
       <h2>상품</h2>
     </Container>
   )
