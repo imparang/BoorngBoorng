581a866aed2561b593bcae5f9a448a2750f3c5cc
imparang <jeongjaeyn@gmail.com>
2021-12-19 01:27:29 +0900

    feat: Create Store router

---
+++ client/src/component/Store.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+
+const Store = () => {
+  return (
+    <Container style={{ height: '600px', marginBottom: '16px' }}>
+      <h1>스토어페이지</h1>
+    </Container>
+  )
+}
+
+export default Store
