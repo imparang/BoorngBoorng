392dcc9ed997ece541c5582eccba7577fe3ef536
imparang <jeongjaeyn@gmail.com>
2021-12-18 15:36:56 +0900

    feat: Create Board router

--- client/src/component/Board.js
+++ client/src/component/Board.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap'
 
 const Board = props => {
   return (
-    <Container style={{ height: '626px' }}>
+    <Container style={{ height: '600px', marginBottom: '16px' }}>
       <h2>게시판</h2>
     </Container>
   )
