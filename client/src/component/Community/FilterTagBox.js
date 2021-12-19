3b48b5faf09b8413976a5887230b92b022b161a8
imparang <jeongjaeyn@gmail.com>
2021-12-19 20:25:16 +0900

    feat: Mark up filter tag box

--- client/src/component/Community/FilterTagBox.js
+++ client/src/component/Community/FilterTagBox.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../../scss/board/filter-box.scss'
 
 const FilterTagBox = () => {
+  const [tags, setTags] = useState([])
+  console.log('FilterTagBox')
   return (
     <div className="filter-box">
       <div className="filter-box-wrapper">
         <i className="bx bx-hash"></i>
-        <ul className="filter-box-list">
-          <li className="filter-box-item">하이</li>
-        </ul>
+        {tags.length ? (
+          <ul className="filter-box-list">
+            <li className="filter-box-item">
+              <button>
+                공지
+                <i className="bx bx-x"></i>
+              </button>
+            </li>
+            <li className="filter-box-item">
+              <button>
+                내용
+                <i className="bx bx-x"></i>
+              </button>
+            </li>
+            <li className="filter-box-item">
+              <button>
+                심심
+                <i className="bx bx-x"></i>
+              </button>
+            </li>
+          </ul>
+        ) : (
+          <span className="filterbox-placeholder">필터링 태그</span>
+        )}
       </div>
       <button className="filter-box-button">
         <i className="bx bx-reset"></i>
-        초기화
+        리셋
       </button>
     </div>
   )
