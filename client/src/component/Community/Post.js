22d88cd530b360b9198e91e0a1d6e5ef945b499d
imparang <jeongjaeyn@gmail.com>
2021-12-21 15:32:21 +0900

    feat: Add Link to navigate post detail component and deliver post data

--- client/src/component/Community/Post.js
+++ client/src/component/Community/Post.js
@@ -1,24 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Post = ({ post }) => {
   return (
-    <article className="post-item">
-      <div className="post-item-main">
-        <h4 className="post-title">{post.title}</h4>
-        <p className="post-content">{post.content}</p>
-        <span className="post-date">
-          {post.insert_date && post.insert_date.slice(0, 16)}
-        </span>
-      </div>
-      <div className="post-item-sub">
-        <div className="user-wrapper">
-          <strong>{post.insert_user}</strong>
+    <article>
+      <Link
+        to={`/board/${post.id}`}
+        state={{ post }}
+        style={{ margin: 0 }}
+        className="post-item"
+      >
+        <div className="post-item-main">
+          <h4 className="post-title">{post.title}</h4>
+          <p className="post-content">{post.content}</p>
+          <span className="post-date">
+            {post.insert_date && post.insert_date.slice(0, 16)}
+          </span>
         </div>
-        <div className="post-view">
-          <i className="bx bx-show-alt"></i>
-          {post.view_count}
+        <div className="post-item-sub">
+          <div className="user-wrapper">
+            <strong>{post.insert_user}</strong>
+          </div>
+          <div className="post-view">
+            <i className="bx bx-show-alt"></i>
+            {post.view_count}
+          </div>
         </div>
-      </div>
+      </Link>
     </article>
   )
 }
