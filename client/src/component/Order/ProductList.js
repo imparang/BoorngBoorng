5cc21340b487d38efe8c34295ac56d719d21cd0d
imparang <jeongjaeyn@gmail.com>
2021-12-21 15:35:19 +0900

    feat: Create ProductList component

---
+++ client/src/component/Order/ProductList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardBody, CardFooter, CardHeader } from 'reactstrap'
+
+const ProductList = () => {
+  return (
+    <Card>
+      <CardHeader>상품 목록</CardHeader>
+      <CardBody></CardBody>
+      <CardFooter>총 결제 금액</CardFooter>
+    </Card>
+  )
+}
+
+export default ProductList
