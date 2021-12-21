861c72486c57598cb030df8719a1314b646be213
imparang <jeongjaeyn@gmail.com>
2021-12-21 16:26:09 +0900

    feat: Create Product List to buy

--- client/src/component/Order/ProductList.js
+++ client/src/component/Order/ProductList.js
@@ -1,11 +1,37 @@
 import React from 'react'
-import { Card, CardBody, CardFooter, CardHeader } from 'reactstrap'
+import { Card, CardBody, CardFooter, CardHeader, Table } from 'reactstrap'
 
 const ProductList = () => {
   return (
     <Card>
       <CardHeader>상품 목록</CardHeader>
-      <CardBody></CardBody>
+      <CardBody>
+        <Table>
+          <tbody>
+            <tr>
+              <th scope="row">이미지</th>
+              <td>상품명</td>
+              <td>제조사</td>
+              <td>상품수</td>
+              <td>가격</td>
+            </tr>
+            <tr>
+              <th scope="row">이미지</th>
+              <td>상품명</td>
+              <td>제조사</td>
+              <td>상품수</td>
+              <td>가격</td>
+            </tr>
+            <tr>
+              <th scope="row">이미지</th>
+              <td>상품명</td>
+              <td>제조사</td>
+              <td>상품수</td>
+              <td>가격</td>
+            </tr>
+          </tbody>
+        </Table>
+      </CardBody>
       <CardFooter>총 결제 금액</CardFooter>
     </Card>
   )
