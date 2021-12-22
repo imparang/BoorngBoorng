4a4518242d4e756d92bd0465093f1355cdec895c
imparang <jeongjaeyn@gmail.com>
2021-12-23 02:07:11 +0900

    feat: Append product select count reducers and sagas

--- client/src/redux/reducers/product.js
+++ client/src/redux/reducers/product.js
@@ -1,17 +1,65 @@
 import {
   PRODUCT_SELECT_REQUEST,
   PRODUCT_SELECT_SUCCESS,
-  PRODUCT_SELECT_FAILURE
+  PRODUCT_SELECT_FAILURE,
+  PRODUCT_COUNT_REQUEST,
+  PRODUCT_COUNT_SUCCESS,
+  PRODUCT_COUNT_FAILURE
 } from '../types'
 
 const initial = {
   productSelectLoading: false,
   productSelectDone: false,
-  productSelectError: null
+  productSelectError: null,
+  productCountLoading: false,
+  productCountDone: false,
+  productCountError: null,
+  products: null,
+  totalCount: 0
 }
 
 const productReducer = (state = initial, action) => {
   switch (action.type) {
+    case PRODUCT_SELECT_REQUEST:
+      return {
+        ...state,
+        productSelectLoading: true,
+        productSelectDone: false,
+        productSelectError: null
+      }
+    case PRODUCT_SELECT_SUCCESS:
+      return {
+        ...state,
+        productSelectLoading: false,
+        productSelectDone: true,
+        products: action.data
+      }
+    case PRODUCT_SELECT_FAILURE:
+      return {
+        ...state,
+        productSelectLoading: false,
+        productSelectError: true
+      }
+    case PRODUCT_COUNT_REQUEST:
+      return {
+        ...state,
+        productCountLoading: true,
+        productCountDone: false,
+        productCountError: null
+      }
+    case PRODUCT_COUNT_SUCCESS:
+      return {
+        ...state,
+        productCountLoading: false,
+        productCountDone: true,
+        totalCount: action.data
+      }
+    case PRODUCT_COUNT_FAILURE:
+      return {
+        ...state,
+        productCountLoading: false,
+        productCountError: true
+      }
     default:
       return state
   }
--- client/src/redux/sagas/product.js
+++ client/src/redux/sagas/product.js
@@ -1,11 +1,15 @@
 import {
   PRODUCT_SELECT_REQUEST,
   PRODUCT_SELECT_SUCCESS,
-  PRODUCT_SELECT_FAILURE
+  PRODUCT_SELECT_FAILURE,
+  PRODUCT_COUNT_SUCCESS,
+  PRODUCT_COUNT_FAILURE,
+  PRODUCT_COUNT_REQUEST
 } from '../types'
-import { all, fork, call, put, takeLatest } from 'react-redux'
+import { all, fork, call, put, takeLatest } from 'redux-saga/effects'
 import getAxiosApi from '../../api/index'
 
+// product selectPage
 function productSelectApi(data) {
   return getAxiosApi('/product?type=page', data)
 }
@@ -29,8 +33,30 @@ function* watchProductSelect() {
   yield takeLatest(PRODUCT_SELECT_REQUEST, productSelect)
 }
 
-const productSaga = () => {
-  all([fork(watchProductSelect)])
+// product selectCount
+function productCountApi(data) {
+  return getAxiosApi('/product?type=count', data)
 }
 
-export default productSaga
+function* productCount(action) {
+  try {
+    const result = yield call(productCountApi, action.data)
+    yield put({
+      type: PRODUCT_COUNT_SUCCESS,
+      data: result
+    })
+  } catch (error) {
+    yield put({
+      type: PRODUCT_COUNT_FAILURE,
+      data: error
+    })
+  }
+}
+
+function* watchProductCount() {
+  yield takeLatest(PRODUCT_COUNT_REQUEST, productCount)
+}
+
+export default function* productSaga() {
+  yield all([fork(watchProductSelect), fork(watchProductCount)])
+}
