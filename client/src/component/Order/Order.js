ee87378d94c9de61aa04b8a5fc853202b02bc2ea
imparang <jeongjaeyn@gmail.com>
2021-12-23 15:33:26 +0900

    feat: Get product data from link and deliver props to child component

--- client/src/component/Order/Order.js
+++ client/src/component/Order/Order.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import { Col, Container, Row } from 'reactstrap'
 import Delivery from './Delivery'
 import Payment from './Payment'
 import ProductList from './ProductList'
 
 const Order = () => {
+  const location = useLocation()
+  const { product } = location.state
   return (
     <Container
       style={{ minHeight: '600px', marginBottom: '16px', position: 'relative' }}
@@ -12,10 +15,10 @@ const Order = () => {
       <h2 className="visually-hidden">상품 구매</h2>
       <Row style={{ marginBottom: '20px' }}>
         <Col>
-          <ProductList />
+          <ProductList product={product} />
         </Col>
       </Row>
-      <Row>
+      <Row style={{ marginBottom: '40px' }}>
         <Col md="6">
           <Delivery />
         </Col>
