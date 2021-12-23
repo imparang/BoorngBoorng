6379b271e58b473a23430401b77e7101762f58c4
imparang <jeongjaeyn@gmail.com>
2021-12-24 00:40:18 +0900

    feat: Append props at Payment of  child components

--- client/src/component/Order/Payment.js
+++ client/src/component/Order/Payment.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Col, Row } from 'reactstrap'
 import { useSelector } from 'react-redux'
 import Delivery from './Delivery'
 import CardInfo from './CardInfo'
 
-const Payment = () => {
+const Payment = ({ product, userId }) => {
   const { cartId } = useSelector(state => state.cart)
+  const [data, setData] = useState({})
+
+  const onChangeData = useCallback(data => {
+    setData(data)
+  }, [])
 
   return (
     <Row style={{ marginBottom: '40px' }}>
       <Col md="6">
-        <Delivery />
+        <Delivery onChangeData={onChangeData} />
       </Col>
       <Col md="6">
-        <CardInfo />
+        <CardInfo
+          cartId={cartId}
+          data={data}
+          product={product}
+          userId={userId}
+        />
       </Col>
     </Row>
   )
