4454d45bf3b7bf56507ef4a21b559ec9228e01fc
imparang <jeongjaeyn@gmail.com>
2021-12-23 15:32:20 +0900

    feat: Set to add post logic with redux

--- client/src/component/Community/AddBoard.js
+++ client/src/component/Community/AddBoard.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap'
 
 import '../../scss/board/add-board.scss'
+import { useInput } from '../../hooks/useInput'
+import { useDispatch } from 'react-redux'
+import { BOARD_ADD_REQUEST } from '../../redux/types'
+import { useNavigate } from 'react-router-dom'
 
 const AddBoard = () => {
+  const dispatch = useDispatch()
+
+  const navigate = useNavigate()
+
+  const [title, onChangeTitle] = useInput('')
+  const [content, onChangeContent] = useInput('')
+  const [writer, onChangeWriter] = useInput('')
+  const [password, onChangePassword] = useInput('')
+
+  const onSubmit = useCallback(
+    async e => {
+      e.preventDefault()
+      await dispatch({
+        type: BOARD_ADD_REQUEST,
+        data: {
+          title,
+          content,
+          insert_user: writer,
+          write_password: password,
+          view_count: Math.ceil(Math.random() * 10)
+        }
+      })
+      navigate('/board')
+    },
+    [title, content, writer, password]
+  )
   return (
     <Container style={{ marginBottom: '16px' }}>
-      <Form className="add-board-form">
+      <Form className="add-board-form" onSubmit={onSubmit}>
         <h2>게시글 등록</h2>
         <FormGroup>
           <Label for="title">Title</Label>
@@ -17,12 +47,21 @@ const AddBoard = () => {
             type="text"
             maxLength={50}
             minLength={1}
+            value={title}
+            onChange={onChangeTitle}
           />
         </FormGroup>
 
         <FormGroup>
           <Label for="content">Content</Label>
-          <Input id="content" name="content" type="textarea" bsSize="lg" />
+          <Input
+            id="content"
+            name="content"
+            type="textarea"
+            bsSize="lg"
+            value={content}
+            onChange={onChangeContent}
+          />
         </FormGroup>
 
         <FormGroup className="insert-user">
@@ -35,6 +74,8 @@ const AddBoard = () => {
               type="text"
               maxLength={30}
               minLength={1}
+              value={writer}
+              onChange={onChangeWriter}
             />
           </FormGroup>
           <FormGroup>
@@ -46,6 +87,8 @@ const AddBoard = () => {
               type="password"
               maxLength={10}
               minLength={1}
+              value={password}
+              onChange={onChangePassword}
             />
           </FormGroup>
         </FormGroup>
