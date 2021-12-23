b31a0b52e6e5a2dfcad65a0deb7a5b9894acec49
imparang <jeongjaeyn@gmail.com>
2021-12-24 00:19:55 +0900

    feat: Implement payment logic with redux and use sweetalert2

--- client/src/component/Order/CardInfo.js
+++ client/src/component/Order/CardInfo.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   Button,
   Card,
@@ -11,25 +12,149 @@ import {
   Input,
   Label
 } from 'reactstrap'
+import { ORDER_COMPLETED_REQUEST, ORDER_REQUEST } from '../../redux/types'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 
-const CardInfo = () => {
+import 'sweetalert2/src/sweetalert2.scss'
+
+const CardInfo = ({ cartId, data, product }) => {
+  const dispatch = useDispatch()
+  const { totalPrice } = useSelector(state => state.cart)
+
+  const [card, setCard] = useState('')
+
+  const nameRef = useRef(null)
+  const cardFirstRef = useRef(null)
+  const cardSecondRef = useRef(null)
+  const cardThirdRef = useRef(null)
+  const cardFourthRef = useRef(null)
+  const cardMonthRef = useRef(null)
+  const cardYearRef = useRef(null)
+
+  const validation = useCallback(() => {
+    if (!nameRef.current.value) {
+      nameRef.current.focus()
+      return false
+    } else if (!cardFirstRef.current.value) {
+      cardFirstRef.current.focus()
+      return false
+    } else if (!cardSecondRef.current.value) {
+      cardSecondRef.current.focus()
+      return false
+    } else if (!cardThirdRef.current.value) {
+      cardThirdRef.current.focus()
+      return false
+    } else if (!cardFourthRef.current.value) {
+      cardFourthRef.current.focus()
+      return false
+    } else if (!cardMonthRef.current.value) {
+      cardMonthRef.current.focus()
+      return false
+    } else if (!cardYearRef.current.value) {
+      cardYearRef.current.focus()
+      return false
+    } else {
+      return true
+    }
+  }, [])
+
+  // TODO 주문 완료 창 한 번 뜨게하자
+  const onSubmit = useCallback(
+    async e => {
+      e.preventDefault()
+      console.log(123)
+      if (validation()) {
+        await dispatch({
+          type: ORDER_REQUEST,
+          data: {
+            ...data,
+            cart_id: cartId,
+            cart_dv: card,
+            card_user: nameRef.current.value,
+            card_number1: cardFirstRef.current.value,
+            card_number2: cardSecondRef.current.value,
+            card_number3: cardThirdRef.current.value,
+            card_number4: cardFourthRef.current.value,
+            card_month: cardMonthRef.current.value,
+            card_year: cardYearRef.current.value,
+            user_id: 'test1234@test.com',
+            total_price: totalPrice,
+            complete_yn: 'Y'
+          }
+        })
+        completed()
+      }
+    },
+    [
+      data,
+      card,
+      cartId,
+      nameRef,
+      cardFourthRef,
+      cardFirstRef,
+      cardSecondRef,
+      cardThirdRef,
+      cardMonthRef,
+      cardYearRef,
+      totalPrice
+    ]
+  )
+
+  const handleOnChange = useCallback(e => {
+    const value = e.target.value
+    setCard(value)
+  }, [])
+
+  const completed = useCallback(() => {
+    Swal.fire({
+      title: '정말 주문하시겠습니까?',
+      text: '주문 후, 반품/환불이 가능합니다.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '주문'
+    })
+      .then(() =>
+        dispatch({
+          type: ORDER_COMPLETED_REQUEST,
+          data: {
+            cart_id: cartId,
+            user_id: 'test1234@test.com',
+            complete_yn: 'Y',
+            product_id: product.product_id
+          }
+        })
+      )
+      .then(result => {
+        if (result.isConfirmed) {
+          Swal.fire('주문이 완료되었습니다.', 'success')
+        }
+      })
+  }, [])
+
   return (
     <Card>
       <CardHeader style={{ fontSize: '16px', fontWeight: '700' }}>
         결제
       </CardHeader>
       <CardBody>
-        <Form>
+        <Form onSubmit={onSubmit}>
           <FormGroup row>
             <Label for="select" sm={2}>
               카드 선택
             </Label>
             <Col sm={10}>
-              <Input id="select" name="select" type="select">
-                <option>KB 나라사랑카드</option>
-                <option>카카오 뱅크 카드</option>
-                <option>우리 V 카드</option>
-                <option>현대 M 카드</option>
+              <Input
+                id="select"
+                name="select"
+                type="select"
+                onChange={handleOnChange}
+              >
+                <option value="kb">KB 나라사랑카드</option>
+                <option value="kakao">카카오 뱅크 카드</option>
+                <option value="woori">우리 V 카드</option>
+                <option value="hyundae">현대 M 카드</option>
               </Input>
             </Col>
           </FormGroup>
@@ -43,7 +168,7 @@ const CardInfo = () => {
                 name="name"
                 placeholder="카드 소유자명을 입력해주세요"
                 type="text"
-                invalid
+                innerRef={nameRef}
               />
             </Col>
           </FormGroup>
@@ -59,7 +184,7 @@ const CardInfo = () => {
                 type="text"
                 minLength={4}
                 maxLength={4}
-                invalid
+                innerRef={cardFirstRef}
               />
               ㅡ
               <Input
@@ -67,7 +192,7 @@ const CardInfo = () => {
                 type="text"
                 minLength={4}
                 maxLength={4}
-                invalid
+                innerRef={cardSecondRef}
               />
               ㅡ
               <Input
@@ -75,7 +200,7 @@ const CardInfo = () => {
                 type="text"
                 minLength={4}
                 maxLength={4}
-                invalid
+                innerRef={cardThirdRef}
               />
               ㅡ
               <Input
@@ -83,7 +208,7 @@ const CardInfo = () => {
                 type="text"
                 minLength={4}
                 maxLength={4}
-                invalid
+                innerRef={cardFourthRef}
               />
             </Col>
           </FormGroup>
@@ -97,21 +222,21 @@ const CardInfo = () => {
                 name="month"
                 type="text"
                 style={{ marginBottom: '10px' }}
-                invalid
+                innerRef={cardMonthRef}
               />
               <Input
                 id="year"
                 name="year"
                 type="text"
                 style={{ marginBottom: '10px' }}
-                invalid
+                innerRef={cardYearRef}
               />
             </Col>
           </FormGroup>
         </Form>
       </CardBody>
       <CardFooter>
-        <Button color="primary" block={true}>
+        <Button color="primary" block={true} onClick={onSubmit}>
           결제
         </Button>
       </CardFooter>
