44ac60b879195c0068a5cffea2a46d9f62f82482
imparang <jeongjaeyn@gmail.com>
2021-12-24 00:21:25 +0900

    feat: Add redirecting logic when you don't have authorization

--- client/src/component/Community/PostContainer.js
+++ client/src/component/Community/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import Post from './Post'
 import { useSelector, useDispatch } from 'react-redux'
 
@@ -8,7 +8,7 @@ import PaginationContainer from '../PaginationContainer'
 import { useNavigate } from 'react-router-dom'
 import { BOARD_SELECT_REQUEST } from '../../redux/types'
 
-const PostContainer = () => {
+const PostContainer = ({ userId }) => {
   const navigate = useNavigate()
 
   const { posts } = useSelector(state => state.board)
@@ -30,17 +30,21 @@ const PostContainer = () => {
       posts?.length && posts.map(post => <Post key={post.id} post={post} />),
     [posts]
   )
+
+  const addPost = useCallback(() => {
+    if (userId) {
+      navigate('/board/insert')
+    } else {
+      window.location.href = '/login'
+    }
+  }, [])
   return (
     <section className="post-list">
       <h3 className="visually-hidden">커뮤니티 포스트 목록</h3>
       {posts?.length ? (
         <>
           {PostList}
-          <Button
-            className="add-button"
-            color="primary"
-            onClick={() => navigate('/board/insert')}
-          >
+          <Button className="add-button" color="primary" onClick={addPost}>
             글쓰기
           </Button>
           <PaginationContainer link={'board'} size="sm" />
