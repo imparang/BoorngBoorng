c8e4ac3cf7c96a8cb7681cf1e8f25035b1d4c6d0
imparang <jeongjaeyn@gmail.com>
2021-12-23 16:45:05 +0900

    feat: Append redux reducer and saga about getting cart id logic

--- client/src/redux/reducers/cart.js
+++ client/src/redux/reducers/cart.js
@@ -1,14 +1,21 @@
 import {
   CART_SELECT_REQUEST,
   CART_SELECT_SUCCESS,
-  CART_SELECT_FAILURE
+  CART_SELECT_FAILURE,
+  CART_ID_REQUEST,
+  CART_ID_SUCCESS,
+  CART_ID_FAILURE
 } from '../types'
 
 const initial = {
   cartSelectLoading: false,
   cartSelectDone: false,
   cartSelectError: null,
-  carts: null
+  cartIdLoading: false,
+  cartIdDone: false,
+  cartIdError: null,
+  carts: null,
+  cartId: null
 }
 
 const cartReducer = (state = initial, action) => {
@@ -32,6 +39,26 @@ const cartReducer = (state = initial, action) => {
         ...state,
         cartSelectLoading: false,
         cartSelectError: true
+      }
+    case CART_ID_REQUEST:
+      return {
+        ...state,
+        cartIdLoading: true,
+        cartIdDone: false,
+        cartIdError: null
+      }
+    case CART_ID_SUCCESS:
+      return {
+        ...state,
+        cartIdLoading: false,
+        cartIdDone: true,
+        cartId: action.data.data.json[0]
+      }
+    case CART_ID_FAILURE:
+      return {
+        ...state,
+        cartIdLoading: false,
+        cartIdError: true
       }
     default:
       return state
--- client/src/redux/sagas/cart.js
+++ client/src/redux/sagas/cart.js
@@ -1,11 +1,15 @@
 import {
   CART_SELECT_REQUEST,
   CART_SELECT_SUCCESS,
-  CART_SELECT_FAILURE
+  CART_SELECT_FAILURE,
+  CART_ID_REQUEST,
+  CART_ID_SUCCESS,
+  CART_ID_FAILURE
 } from '../types'
 import { all, fork, call, put, takeLatest } from 'redux-saga/effects'
 import getAxiosApi from '../../api'
 
+// cart select list
 function cartSelectApi(data) {
   return getAxiosApi('/cart?type=list', data)
 }
@@ -29,6 +33,30 @@ function* watchCartSelect() {
   yield takeLatest(CART_SELECT_REQUEST, cartSelect)
 }
 
+// cart cart id
+function cartIdApi(data) {
+  return getAxiosApi('/cart?type=cart_id', data)
+}
+
+function* cartId(action) {
+  try {
+    const result = yield call(cartIdApi, action.data)
+    yield put({
+      type: CART_ID_SUCCESS,
+      data: result
+    })
+  } catch (error) {
+    yield put({
+      type: CART_ID_FAILURE,
+      data: error
+    })
+  }
+}
+
+function* watchCartId() {
+  yield takeLatest(CART_ID_REQUEST, cartId)
+}
+
 export default function* cartSaga() {
-  yield all([fork(watchCartSelect)])
+  yield all([fork(watchCartSelect), fork(watchCartId)])
 }
